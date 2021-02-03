0b96ab80e22046b678e982ae53b28c6f9cb1f0fb
Hasan Gharaibeh <swehu1988@gmail.com>
2021-02-03 15:53:55 +1100

    Methods imp added

---
+++ ConsoleApp1/ConsoleApp1/FrequentArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FrequentArray
+    {
+        public void findLongest(int[] numArray)
+        {
+            List<Tuple<int, int>> CustomDictionary = new List<Tuple<int, int>>();
+
+            foreach (int num in numArray)
+            {
+                CustomDictionary.Add(new Tuple<int, int>(num, numArray.Count(x => x == num)));
+            }
+             
+            foreach (var pair in CustomDictionary)
+            {
+                Console.WriteLine("FOREACH VAR: {0}, {1}", pair.Item1, pair.Item2);
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            FrequentArray freq = new FrequentArray();
+
+            int[] arr = new int[] { 5, 4, 3, 2, 4, 5, 1, 6, 1, 2, 5, 4 };
+            //int[] arr = new int[] { 1, 4, 6, 7};
+            freq.findLongest(arr);
+            Console.ReadLine();
+
         }
     }
 }
